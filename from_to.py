6922f4600eeed8e549e1491eb657b01cd1d8cc8a
delsyke <wasykepaul@gmail.com>
2023-12-05 22:14:22 +0300

    modified code to write time in excel in the format HH:MM. Also added additional destinations and timings

--- from_to.py
+++ from_to.py
@@ -2,6 +2,7 @@ from openpyxl import load_workbook
 from routes import get_route, take_routes
 from datetime import datetime, timedelta, date
 from times import calculate_times, get_eta
+##from validators import aircraft_validator
 
 
 today = date.today()
@@ -13,8 +14,10 @@ with open('departure_times.txt') as f:
 departure_times = times.split(' ') #list of all etds for each route in routings.txt
 
 
+
 wb = load_workbook('log.xlsx')
 sheet = wb['FF']
+sheet2 = wb['CONFIG']
 
 i = 5 #start row
 t = 0 #start index of departure_times
@@ -36,13 +39,30 @@ for stops in routes:
 
         sheet['E'+row].value = dep
         sheet['F'+row].value = dest
-        sheet['I'+row].value = f'{eta.hour}:{eta.minute}'
+
+        eta_hr_str = str(eta.hour)
+        eta_min_str = str(eta.minute)
+
+        if len(eta_hr_str) == 1:
+            eta_hr_str = '0' + eta_hr_str
+        if len(eta_min_str) == 1:
+            eta_min_str = '0' + eta_min_str
+
+        sheet['I'+row].value = f'{eta_hr_str}:{eta_min_str}'
 
         ground_time = timedelta(minutes=10)
         departure_time = eta + ground_time
 
+        dep_time_hr_str = str(departure_time.hour)
+        dep_time_min_str = str(departure_time.minute)
+
+        if len(dep_time_hr_str) == 1:
+            dep_time_hr_str = '0' + dep_time_hr_str
+        if len(dep_time_min_str) == 1:
+            dep_time_min_str = '0' + dep_time_min_str
+
         i+=1
-        sheet['H'+str(i)].value = f'{departure_time.hour}:{departure_time.minute}'
+        sheet['H'+str(i)].value = f'{dep_time_hr_str}:{dep_time_min_str}'
         r+=1
 
     sheet['H'+str(i)].value = ''
@@ -50,4 +70,6 @@ for stops in routes:
     t+=1
 
 
-wb.save(f'FLIGHT FOLLOWING LOG {filename}.xlsx')
+##aircraft_validator(sheet)
+
+wb.save(f'FF LOG {filename}.xlsx')
